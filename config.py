8cb59ce110aa70e231ac02664c2426eaa1ddaec7
Tanner Gower <tanner.gower@gmail.com>
2018-10-29 19:37:25 -0500

    fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-#mysql+pymysql://dayzliveweb:XuytQuwk3-Cifo@localhost/dayzlist?unix_socket=/var/run/mysqld/mysqld.sock
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://tanner:Gomwer5676567``@localhost:3306/dayzlist'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dayzliveweb:XuytQuwk3-Cifo@localhost/dayzlist?unix_socket=/var/run/mysqld/mysqld.sock'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 WTF_CSRF_ENABLED = True
